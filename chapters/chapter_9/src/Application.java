a38b046d50831aaf563a9f32146dd01eb0205829
Artyom Tarasov <laflenory@gmail.com>
2023-07-24 14:51:08 +0300

    [*] Added chapter_9 & Update README.md file

--- chapters/chapter_9/src/Application.java
+++ chapters/chapter_9/src/Application.java
@@ -1,3 +1,24 @@
-public class Application {
+import java.util.Scanner;
 
+public class Application {
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+
+        int flag = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                flag += 1;
+            }
+        }
+
+        if (flag < 2 && number > 1) {
+            System.out.println("Число является простым.");
+        } else {
+            System.out.println("Число не является простым.");
+        }
+    }
 }
