3f43215bd32ca1f1e85f391164a82a1301b14365
Artyom Tarasov <laflenory@gmail.com>
2023-07-24 11:24:23 +0300

    [*] Added chapter_13 & Update README.md file

--- chapters/chapter_13/src/Application.java
+++ chapters/chapter_13/src/Application.java
@@ -1,3 +1,37 @@
-public class Application {
+import java.util.Arrays;
+import java.util.Scanner;
+import java.text.MessageFormat;
 
+public class Application {
+    public static int min(int[] numbers, int length) {
+        int min = numbers[0];
+
+        for (int i = 1; i < length; i++) {
+            if (min > numbers[i]) {
+                min = numbers[i];
+            }
+        }
+
+        return min;
+    }
+
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите размер массива: ");
+        final int length = scanner.nextInt();
+        int[] numbers = new int[length];
+
+        String pattern = "Введите число под номером {0}: ";
+
+        for (int i = 0; i < length; i++) {
+            System.out.print(MessageFormat.format(pattern, i + 1));
+            int number = scanner.nextInt();
+
+            numbers[i] = number;
+        }
+
+        System.out.println("Полученный массив: " + Arrays.toString(numbers));
+        System.out.println("Минимальное число массива: " + min(numbers, length));
+    }
 }
