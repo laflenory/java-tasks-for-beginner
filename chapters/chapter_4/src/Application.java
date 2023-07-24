300a1fbf84eb9fe80dec83b11346275de15154b0
Artyom Tarasov <laflenory@gmail.com>
2023-07-24 11:11:29 +0300

    [*] Added chapter_4 & Update README.md file

--- chapters/chapter_4/src/Application.java
+++ chapters/chapter_4/src/Application.java
@@ -1,3 +1,16 @@
-public class Application {
+import java.util.Scanner;
 
+public class Application {
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Заданное число является чётным.");
+        } else {
+            System.out.println("Заданное число является нечётным.");
+        }
+    }
 }
