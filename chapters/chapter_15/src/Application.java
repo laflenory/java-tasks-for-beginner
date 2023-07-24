0beb71788e0f9aa07f27c596156a63c9b0b1b849
Artyom Tarasov <laflenory@gmail.com>
2023-07-24 15:07:46 +0300

    [*] Added chapter_15 & Update README.md file

--- chapters/chapter_15/src/Application.java
+++ chapters/chapter_15/src/Application.java
@@ -1,3 +1,24 @@
-public class Application {
+import java.util.Scanner;
 
+public class Application {
+    public static int sum(String number) {
+        String[] array = number.split("");
+
+        int result = 0;
+
+        for (String character : array) {
+            result += Integer.parseInt(character);
+        }
+
+        return result;
+    }
+
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число: ");
+        String number = scanner.nextLine();
+
+        System.out.print("Сумма цифр числа: " + sum(number));
+    }
 }
