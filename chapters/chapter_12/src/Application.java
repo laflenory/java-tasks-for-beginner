bf3377cfec65abab2c8e0a0403156fc1cdd99c24
Artyom Tarasov <laflenory@gmail.com>
2023-07-24 14:33:38 +0300

    [*] Added chapter_12 & Update README.md file

--- chapters/chapter_12/src/Application.java
+++ chapters/chapter_12/src/Application.java
@@ -1,34 +1,3 @@
-import java.util.Scanner;
-
 public class Application {
-    public static String revert(String string) {
-        char[] characters = string.toCharArray();
 
-/*
-        StringBuilder result = new StringBuilder();
-
-        for (int i = characters.length - 1; i >= 0; i--) {
-            result.append(characters[i]);
-        }
-
-        return result.toString();
-*/
-
-        String result = "";
-
-        for (int i = characters.length - 1; i >= 0 ; i--) {
-            result += characters[i];
-        }
-
-        return result;
-    }
-
-    public static void main(String []args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите строку: ");
-        String string = scanner.nextLine();
-
-        System.out.println("Результат: " + revert(string));
-    }
 }
