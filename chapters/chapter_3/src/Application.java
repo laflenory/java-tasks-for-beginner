46d5e37f53bbac43711100dc3151c3062b52e2de
Artyom Tarasov <laflenory@gmail.com>
2023-07-24 10:52:44 +0300

    [*] Added chapter_3 & Update README.md file

--- chapters/chapter_3/src/Application.java
+++ chapters/chapter_3/src/Application.java
@@ -1,3 +1,36 @@
-public class Application {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Application {
+    public static int max(int[] numbers, int length) {
+        int max = numbers[0];
+
+        for (int i = 1; i < length; i++) {
+            if (max < numbers[i]) {
+                max = numbers[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int current = 0;
+
+        final int length = 3;
+        int[] numbers = new int[length];
+
+        while (current != length) {
+            System.out.print("Введите число: ");
+            int number = scanner.nextInt();
+
+            numbers[current] = number;
+            current++;
+        }
+
+        System.out.print("Максимальное число: ");
+        System.out.println(max(numbers, length));
+    }
 }
