4a0bc67a62fe0ae54ce783c17d2788025ccc6e48
Artyom Tarasov <laflenory@gmail.com>
2023-07-24 19:48:31 +0300

    [*] Added chapter_14 & Update README.md file

--- chapters/chapter_14/src/Application.java
+++ chapters/chapter_14/src/Application.java
@@ -1,3 +1,28 @@
-public class Application {
+import java.util.Objects;
+import java.util.Scanner;
 
+public class Application {
+    public static String coup(String string) {
+        String[] array = string.split("");
+        String output = "";
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            output += array[i];
+        }
+
+        return output;
+    }
+
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        String string = scanner.nextLine();
+
+        if (Objects.equals(string, coup(string))) {
+            System.out.println("Заданное слово является палиндромом.");
+        } else {
+            System.out.println("Заданное слово не является палиндромом.");
+        }
+    }
 }
