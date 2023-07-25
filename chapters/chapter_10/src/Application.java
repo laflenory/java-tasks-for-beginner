1086c710271e07d89034671b032fc3baa1bd38b0
Artyom Tarasov <laflenory@gmail.com>
2023-07-25 21:08:55 +0300

    [*] Added chapter_10 & Update README.md file

--- chapters/chapter_10/src/Application.java
+++ chapters/chapter_10/src/Application.java
@@ -1,3 +1,17 @@
-public class Application {
+import java.util.Scanner;
 
+public class Application {
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int first_cathetus, second_cathetus;
+
+        System.out.print("Введите первый катет: ");
+        first_cathetus = scanner.nextInt();
+
+        System.out.print("Введите второй катет: ");
+        second_cathetus = scanner.nextInt();
+
+        System.out.println("Полученная гипотенуза: " + Math.sqrt(Math.pow(first_cathetus, 2) + Math.pow(second_cathetus, 2)));
+    }
 }
