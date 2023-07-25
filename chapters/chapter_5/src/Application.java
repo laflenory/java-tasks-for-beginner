05566e4558f106103be703bd60ab16ab2bdda555
Artyom Tarasov <laflenory@gmail.com>
2023-07-25 21:15:36 +0300

    [*] Added chapter_5 & Update README.md file

--- chapters/chapter_5/src/Application.java
+++ chapters/chapter_5/src/Application.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Application {
     public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Введите радиус круга: ");
+        int radius = scanner.nextInt();
+
+        System.out.println("Площадь круга: " + (Math.PI * Math.pow(radius, 2)));
     }
 }
