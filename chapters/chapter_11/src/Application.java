bb8b51519b6db7fe563c52d1abe8fe82b382c4ed
Artyom Tarasov <laflenory@gmail.com>
2023-07-26 15:27:02 +0300

    [*] Added chapter__1 & Update README.md file

--- chapters/chapter_11/src/Application.java
+++ chapters/chapter_11/src/Application.java
@@ -1,3 +1,34 @@
-public class Application {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Application {
+    public static void main(String[] args) {
+        final String[] vowels = { "A", "E", "I", "O", "U" };
+        final String[] consonants = {
+                "B", "C", "D", "F",
+                "G", "H", "J", "K",
+                "L", "M", "N", "P",
+                "Q", "R", "S", "T",
+                "V", "W", "X", "Y",
+                "Z"
+        };
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        String string = scanner.nextLine();
+
+        int vowelsCount = 0, consonantsCount = 0;
+
+        for (String letter : string.split("")) {
+            if (Arrays.stream(vowels).toList().contains(letter.toUpperCase())) {
+                vowelsCount += 1;
+            } else if (Arrays.stream(consonants).toList().contains(letter.toUpperCase())) {
+                consonantsCount += 1;
+            }
+        }
+
+        System.out.println("Гласные буквы: " + vowelsCount);
+        System.out.println("Согласные буквы: " + consonantsCount);
+    }
 }
